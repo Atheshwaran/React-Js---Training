3548febc545f7cce49195029e65e2ab7996a0b0b
Atheshwaran K <61022773+Atheshwaran@users.noreply.github.com>
2021-06-10 12:00:35 +0530

    updated

--- Cricket-Score/src/Reducers/ScoreA.js
+++ Cricket-Score/src/Reducers/ScoreA.js
@@ -1,7 +1,7 @@
 const scoreA = (score = 0, action) => {
     switch (action.type) {
-        case "IncrementA":
-            return Number(score) + Number(action.run);
+        case "IncrementSagaA":
+            return Number(action.run);
         default:
             return score;
     }
